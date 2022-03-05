179a12300bdfe038db302abd0141dac715425493
MichaIng <micha@dietpi.com>
2022-03-05 15:26:04 +0100

    Remove duplicate assets

--- resources/views/ssh_login.blade.php
+++ resources/views/ssh_login.blade.php
@@ -2,9 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -81,12 +79,10 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
                 <div>
                     <form action="{{ url('/ssh-login') }}" method="Post">
                         {{ csrf_field() }}
