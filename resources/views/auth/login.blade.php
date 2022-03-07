7f572f4cb0b92d6aad2a0c7bef4b7196b5aa9803
MichaIng <micha@dietpi.com>
2022-03-07 01:04:58 +0100

    Fixes (WIP)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,8 +5,8 @@
     <div class="container">
         @if(Session::has('custom_message'))
             <div class="alert alert-success" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-            <span>{!! Session::get('custom_message') !!}</span>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+                <span>{!! Session::get('custom_message') !!}</span>
             </div>
         @endif
         <div class="cont">
@@ -17,13 +17,10 @@
                         <div class="panel-body">
                             <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                                 {{ csrf_field() }}
-
                                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                                     <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
                                     <div class="col-md-6">
                                         <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
                                         @if ($errors->has('email'))
                                             <span class="help-block">
                                                 <strong>{{ $errors->first('email') }}</strong>
@@ -31,21 +28,15 @@
                                         @endif
                                     </div>
                                 </div>
-
                                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                     <label for="password" class="col-md-4 control-label">Password</label>
-
                                     <div class="col-md-6">
                                         <input id="password" type="password" class="form-control" name="password" required>
-
                                         @if ($errors->has('password'))
-                                            <span class="help-block">
-                                                <strong>{{ $errors->first('password') }}</strong>
-                                            </span>
+                                            <span class="help-block"><strong>{{ $errors->first('password') }}</strong></span>
                                         @endif
                                     </div>
                                 </div>
-
                                 <div class="form-group">
                                     <div class="col-md-6 col-md-offset-4">
                                         <div class="checkbox">
@@ -56,15 +47,11 @@
                                                 Forgot Your Password?
                                             </a> -->
                                         </div>
-
                                     </div>
                                 </div>
-
                                 <div class="form-group">
                                     <div class="col-md-8 col-md-offset-4">
-                                        <button type="submit" class="btn white_btn">
-                                            Login
-                                        </button>
+                                        <button type="submit" class="btn white_btn">Login</button>
                                     </div>
                                 </div>
                             </form>
