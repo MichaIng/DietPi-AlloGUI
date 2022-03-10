2fe98debf58082f2dbb67e96cfad14f797b7ba94
MichaIng <micha@dietpi.com>
2022-03-10 05:03:54 +0100

    Frontent enhancements
    Use a single Bootstraps stylesheet for login page and content pages.
    
    Enable content container min-height for content pages as well, not only for login page. This removes the white border at the bottom of small pages like Squeezelite page. Use the "header-outr" consistently for this, which is applied to the header on all pages.
    
    Fix collapse status indicator on status page.
    
    Fix collapse indicator position on full width headers.
    
    Remove obsolete scripts.sh.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -14,20 +14,19 @@
                     <p><?php echo exec("TERM=linux; . /boot/dietpi/.hw_model; echo \$G_HW_MODEL_NAME"); ?></p>
                     <div class="date"><span class="glyphicon glyphicon-calendar" aria-hidden="true">&nbsp;</span><?php echo "$current_date"; ?></div>
                 </div>
-
                 <div class="col-md-7 col-sm-12 col-xs-12 header-right">
                     <div class="col-md-4 col-sm-4 col-xs-12 right-dropmenu">
-                      <span><img src="{{ asset('img/user-icon.png') }}"></span>
-                      <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-                        {{ Auth::user()->name }} <span class="fa fa-caret-down" aria-hidden="true"></span>
-                      </a>
-                      <ul class="dropdown-menu">
-                       <li>
-                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
-                       </li>
-                      </ul>
-                     </div>
+                        <span><img src="{{ asset('img/user-icon.png') }}"></span>
+                        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                            {{ Auth::user()->name }} <span class="fa fa-caret-down" aria-hidden="true"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
